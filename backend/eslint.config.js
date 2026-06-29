34ca597ae055237727c33d7c62a120dad7c286f7
0101chaitanya <0101chaitanya@gmail.com>
2026-06-29 11:23:04 +0530

    Backend setup done

---
+++ backend/eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import configPrettier from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  configPrettier, // Disables ESLint rules that might conflict with Prettier
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module", // Use "commonjs" if you are using require() instead of import
+      globals: {
+        ...globals.node,
+        ...globals.jest, // Optional: Include if you use Jest for testing
+      },
+    },
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // Triggers Prettier formatting issues as ESLint errors
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off", // Usually off for Express apps to allow console.log/error
+      "prefer-const": "error",
+    },
+  },
+];
